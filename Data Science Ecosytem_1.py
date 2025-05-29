f522a91d6caa20b4afff4ddaee6a3ff62246ec2c
Haseebu988 <haseebu988@gmail.com>
2025-05-29 08:46:14 +0500

    Add files via upload

---
+++ Data Science Ecosytem_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Auther Names in H2 Style
+# # Lakshmi Hola
+# # Niveditha Pandith
+# 
+# ## other contributers are
+# ## Rav Ahija
+# ## Malika Singla
+# 
+# ### My name is Haseeb Ullah
+
+# In[ ]:
+
+
+
+
